bd19cf1159b8a429ddcafd662397c3d876cc515a
tortuloca <tortuloca@gmail.com>
2023-05-16 21:24:10 -0600

    rotating navigation done

--- Rotating Navigation/script.js
+++ Rotating Navigation/script.js
@@ -1,2 +1,10 @@
 const open = document.getElementById('open')
 const close = document.getElementById('close')
+const container = document.querySelector('.container')
+const circle = document.querySelector('.circle-container')
+
+open.addEventListener('click', () => container.classList.add('show-nav'))
+open.addEventListener('click', () => circle.classList.add('show-nav'))
+
+close.addEventListener('click', () => container.classList.remove('show-nav'))
+close.addEventListener('click', () => circle.classList.remove('show-nav'))
